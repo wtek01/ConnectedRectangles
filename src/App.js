a232a7cfff18d40d6a6ee4dcc0512894bd1a5170
wtek01 <wtek01@gmail.com>
2023-03-24 09:07:12 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+const Rectangles = () => {
+  return (
+    <div className="container">
+      <div className="rectangle"></div>
+      <div className="line"></div>
+      <div className="rectangle"></div>
+      <div className="line"></div>
+      <div className="rectangle"></div>
+    </div>
+  );
+};
+
+export default Rectangles;
